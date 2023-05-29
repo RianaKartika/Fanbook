aab09cd55db0c69b3e88b4b9a303763ddff93dcf
RianaKartika <rianakartika03@gmail.com>
2023-05-29 10:24:13 +0700

    app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ from pymongo import MongoClient
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get("MONGODB_URI")
+MONGODB_URI= os.environ.get("MONGODB_URI")
 DB_NAME = os.environ.get("DB_NAME")
 
-client = MongoClient('MONGODB_URI')
+client = MongoClient (MONGODB_URI)
 
-db = client[DB_NAME]
+db = client [DB_NAME]
 
 app = Flask(__name__)
 
